42bf65064cc8202a4ca29e8d7ffc614e2333063c
ysq <easylover36@163.com>
2022-09-21 00:48:31 +0800

    xargs fixed

--- user/xargs.c
+++ user/xargs.c
@@ -9,24 +9,26 @@ int main(int argc, char* argv[]){
     for(i = 0 ; i < argc - 1 ; i++){
         arg[i] = argv[i + 1];
     }
-    arg[i+1] = 0;
+    arg[argc] = 0;
     memset(buf,0,sizeof(buf));
-    while(read(0,buf + idx,1) > 0){
-        //printf("%s\n",buf);
-        if(buf[idx] == '\n'){
-            buf[idx] = '\0';
-            arg[i] = p;
-            if(fork() == 0){
-                exec(argv[1],arg);
-                exit(0);
+    
+        while(read(0,buf + idx,1) > 0){
+            //printf("%s\n",buf);
+            if(buf[idx] == '\n'){
+                buf[idx] = '\0';
+                arg[i] = p;
+                if(fork() == 0){
+                    exec(argv[1],arg);
+                    exit(0);
+                }
+                else{
+                    wait(0);
+                }
+                //printf("exec complete!\n");
+                p = buf + idx + 1;
             }
-            else{
-                wait(0);
-            }
-            //printf("exec complete!\n");
-            p = buf + idx + 1;
+            idx++;
         }
-        idx++;
-    }
+    
     exit(0);
 }
